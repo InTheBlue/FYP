e05b6940be90472d070b8f9ae179326ee54e7db4
Daniel Wright <danwright91@gmail.com>
2014-07-22 19:34:42 +0000

    Moved loop out of test function

--- TestProgram.cpp
+++ TestProgram.cpp
@@ -8,21 +8,21 @@ using namespace std;
 #include "cryptopp/sha.h"
 #include "cryptopp/base64.h"
 
-void doWork()
+void doWork(long count)
 {
-	for(long i = 0; i < 10000000; i++)
-	{
-		CryptoPP::SHA512 hash;
-		string digest;
-		ostringstream oss;
-		oss << "Testing" << i;
-		string msg = oss.str();
-		CryptoPP::StringSource s(msg, true, new CryptoPP::HashFilter(hash, new CryptoPP::Base64Encoder( new CryptoPP::StringSink(digest))));
-	}
+	CryptoPP::SHA512 hash;
+	string digest;
+	ostringstream oss;
+	oss << "Testing" << count;
+	string msg = oss.str();
+	CryptoPP::StringSource s(msg, true, new CryptoPP::HashFilter(hash, new CryptoPP::Base64Encoder( new CryptoPP::StringSink(digest))));
 }
 int main( int, char** ) {
 	chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
-	doWork();
+	for(long i = 0; i < 10000000; i++)
+	{
+		doWork(i);
+	}
 	chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
 	chrono::duration<double> elapsed = chrono::duration_cast<chrono::duration<double>>(t2-t1);
 	cout << elapsed.count() << endl;
